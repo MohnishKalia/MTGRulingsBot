dd66722f8f6691c77043fe8f1f30441ac1cdcec7
Mohnish Kalia <mohnish.kalia@gmail.com>
2025-06-25 03:50:04 -0500

    Remove '/register' from middleware config and fix regex pattern for path matching

--- middleware.ts
+++ middleware.ts
@@ -47,14 +47,13 @@ export const config = {
     '/chat/:id',
     '/api/:path*',
     '/login',
-    '/register',
-
+    
     /*
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt, etc. (metadata files)
      */
-    '/((?!api/auth|_next/static|_next/image|static|fonts|android-chrome-192x192.png|android-chrome-512x512.png|apple-touch-icon.png|favicon-16x16.png|favicon-32x32.png|favicon.ico|logo.png|logo.svg|robots.txt|site.webmanifest|sitemap-0.xml|sitemap.xml).+)', // Exclude static assets and all public files
+    '/((?!api/auth|_next/static|_next/image|static|fonts|android-chrome-192x192.png|android-chrome-512x512.png|apple-touch-icon.png|favicon-16x16.png|favicon-32x32.png|favicon.ico|logo.png|logo.svg|robots.txt|site.webmanifest|sitemap-0.xml|sitemap.xml).*)', // Exclude static assets and all public files
   ],
 };
